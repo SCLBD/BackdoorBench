f36c93daaebc53e094a53ddcdfce54a2a8f823f1
mdzhang <55868217+mdzhangst@users.noreply.github.com>
2022-03-24 12:30:04 +0800

    Update load_result_rc.py

--- defense/load_result_rc.py
+++ defense/load_result_rc.py
@@ -64,43 +64,9 @@ if __name__ == '__main__':
     save_path = './record/' + args.result_file
     
     result = torch.load(save_path + '/' + args.defense + '/defense_result.pt')
-    print('asr:{} acc:{}'.format(result['asr'],result['acc']))
+    print('asr:{} acc:{} rc:{}'.format(result['asr'],result['acc'],result['rc']))
 
-    #####rc#######仅适用于cifar10
-    print(save_path + '/attack_result.pt')
-    data = load_attack_result(save_path + '/attack_result.pt')
-    ori_label_un = data['clean_test']['y']
-    ori_label = [i for i in ori_label_un if i != 0]
-    try:
-        model = generate_cls_model(result['model_name'],10)
-        model.load_state_dict(result['model'])
-    except RuntimeError:
-        model = generate_cls_model('preactresnet18',10)
-        model.load_state_dict(result['model'])
-    model.to('cuda')
-    #data_set = get_dataset_train(args)
-    tran = get_transform('cifar10', *([32,32]) , train = False)
-    x = torch.tensor(nCHW_to_nHWC(data['bd_test']['x'].detach().numpy()))
-    y = torch.tensor(ori_label)
-    print('y:{}'.format(y.size(0)))
-    print('x:{}'.format(x.size(0)))
-    data_set = torch.utils.data.TensorDataset(x,y)
-    data_set_o = prepro_cls_DatasetBD(
-        full_dataset_without_transform=data_set,
-        poison_idx=np.zeros(len(data_set)),  # one-hot to determine which image may take bd_transform
-        bd_image_pre_transform=None,
-        bd_label_pre_transform=None,
-        ori_image_transform_in_loading=tran,
-        ori_label_transform_in_loading=None,
-        add_details_in_preprocess=False,
-    )
-    data_loader = torch.utils.data.DataLoader(data_set_o, batch_size=128, num_workers=1, shuffle=True)
-    robust_acc = 0
-    for i, (inputs,labels) in enumerate(data_loader):  # type: ignore
-        inputs, labels = inputs.to('cuda'), labels.to('cuda')
-        outputs = model(inputs)
-        pre_label = torch.max(outputs,dim=1)[1]
-        robust_acc += torch.sum(pre_label == labels)/len(data_set_o)
+    
 
     ########### CSV ###########
     # df_head = ['Attack', 'Defense', 'Model', 'ACC', 'ASR']
@@ -115,7 +81,7 @@ if __name__ == '__main__':
     ########### CSV ###########
     df_head = ['Attack', 'Defense', 'Model', 'ACC', 'ASR', 'RC']
     df = pd.DataFrame(columns=df_head)
-    df.loc[df.shape[0]] = {'Attack':save_path.split('/')[-1], 'Defense': args.defense, 'Model':result['model_name'], 'ACC': result['acc'].data.item(), 'ASR': result['asr'].data.item(), 'RC': robust_acc.cpu().numpy()}
+    df.loc[df.shape[0]] = {'Attack':save_path.split('/')[-1], 'Defense': args.defense, 'Model':result['model_name'], 'ACC': result['acc'].data.item(), 'ASR': result['asr'].data.item(), 'RC': result['rc'].data.item()}
     
     log_file='./result.csv'
     if os.path.isfile(log_file):
