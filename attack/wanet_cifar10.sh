f9e33130845593f74bd8786aa9b5b9c6888f1fa4
chr <chen_ray@outlook.com>
2022-03-26 19:39:47 +0800

    delete program in progress

--- attack/wanet_cifar10.sh
+++
@@ -1,5 +0,0 @@
-python wanetAttack.py --pc 0.1 --save_folder_name wanetAttack_0_1
-python wanetAttack.py --pc 0.05 --save_folder_name wanetAttack_0_05
-python wanetAttack.py --pc 0.01 --save_folder_name wanetAttack_0_01
-python wanetAttack.py --pc 0.005 --save_folder_name wanetAttack_0_005
-python wanetAttack.py --pc 0.001 --save_folder_name wanetAttack_0_001
