f13bd6333180df488c825fd1edc8cfdbd65d86a3
chr <chen_ray@outlook.com>
2022-04-20 15:24:06 +0800

    add kwargs for generate_cls_model, to improve the flexibility of model generation.

--- utils/aggregate_block/model_trainer_generate.py
+++ utils/aggregate_block/model_trainer_generate.py
@@ -14,6 +14,7 @@ from utils.trainer_cls import ModelTrainerCLS
 def generate_cls_model(
     model_name: str,
     num_classes: int = 10,
+    **kwargs,
 ):
     '''
     # idea: aggregation block for selection of classifcation models
@@ -22,37 +23,38 @@ def generate_cls_model(
     :return:
     '''
     if model_name == 'resnet18':
-        net = resnet18(num_classes=num_classes, pretrained=False)
+        net = resnet18(num_classes=num_classes, **kwargs)
     elif model_name == 'preactresnet18':
+        assert len(kwargs.keys()) == 0  # means we do NOT allow any kwargs in this case !!!
         logging.warning('Make sure you want PreActResNet18, which is NOT resnet18.')
         from models.preact_resnet import PreActResNet18
         net = PreActResNet18(num_classes=num_classes)
     elif model_name == 'resnet34':
-        net = resnet34(num_classes=num_classes, pretrained=False)
+        net = resnet34(num_classes=num_classes, **kwargs)
     elif model_name == 'alexnet':
-        net = models.alexnet(num_classes= num_classes)
+        net = models.alexnet(num_classes= num_classes, **kwargs)
     elif model_name == 'vgg16':
-        net = models.vgg16(num_classes= num_classes)
+        net = models.vgg16(num_classes= num_classes, **kwargs)
     elif model_name == 'vgg19':
-        net = models.vgg19(num_classes = num_classes)
+        net = models.vgg19(num_classes = num_classes, **kwargs)
     elif model_name == 'squeezenet1_0':
-        net = models.squeezenet1_0(num_classes= num_classes)
+        net = models.squeezenet1_0(num_classes= num_classes, **kwargs)
     elif model_name == 'densenet161':
-        net = models.densenet161(num_classes= num_classes)
+        net = models.densenet161(num_classes= num_classes, **kwargs)
     elif model_name == 'inception_v3':
-        net = models.inception_v3(num_classes= num_classes)
+        net = models.inception_v3(num_classes= num_classes, **kwargs)
     elif model_name == 'googlenet':
-        net = models.googlenet(num_classes= num_classes)
+        net = models.googlenet(num_classes= num_classes, **kwargs)
     elif model_name == 'shufflenet_v2_x1_0':
-        net = models.shufflenet_v2_x1_0(num_classes= num_classes)
+        net = models.shufflenet_v2_x1_0(num_classes= num_classes, **kwargs)
     elif model_name == 'mobilenet_v2':
-        net = models.mobilenet_v2(num_classes= num_classes)
+        net = models.mobilenet_v2(num_classes= num_classes, **kwargs)
     elif model_name == 'resnext50_32x4d':
-        net = models.resnext50_32x4d(num_classes= num_classes)
+        net = models.resnext50_32x4d(num_classes= num_classes, **kwargs)
     elif model_name == 'wide_resnet50_2':
-        net = models.wide_resnet50_2(num_classes= num_classes)
+        net = models.wide_resnet50_2(num_classes= num_classes, **kwargs)
     elif model_name == 'mnasnet1_0':
-        net = models.mnasnet1_0(num_classes= num_classes)
+        net = models.mnasnet1_0(num_classes= num_classes, **kwargs)
     else:
         raise SystemError('NO valid model match in function generate_cls_model!')
 
