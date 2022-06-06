abcc2f9f6d1223b088334ce687d15fec7e7d6372
Shawkui <shaokui.w@gmail.com>
2022-06-06 16:49:59 +0800

    change record directory

--- visualization/demo.sh
+++ visualization/demo.sh
@@ -3,5 +3,5 @@
 for base_model in "preactresnet18" "vgg19" "efficientnet_b3" "mobilenet_v3_large" "densenet161"
 do
     CUDA_VISIBLE_DEVICES=1 python ./visualization/visualize.py --dataset_path ./data --dataset tiny --result_file_attack tiny_${base_model}_badnet_0_05 --model ${base_model}
-    CUDA_VISIBLE_DEVICES=1 python ./visualization/visualize.py --dataset_path ./data --dataset tiny --result_file_attack tiny_${base_model}_badnet_0_05 --result_file_defense badnet_${base_model}_tiny_0_05/anp --model ${base_model}
+    CUDA_VISIBLE_DEVICES=1 python ./visualization/visualize.py --dataset_path ./data --dataset tiny --result_file_attack tiny_${base_model}_badnet_0_05 --result_file_defense tiny_${base_model}_badnet_0_05/anp --model ${base_model}
 done
