1c49d9ce9388dbe1921bd5eac8a99308275ba650
chr <chen_ray@outlook.com>
2022-06-16 18:06:11 +0800

    add license for lc and lf attack

--- attack/lf_attack.py
+++ attack/lf_attack.py
@@ -1,5 +1,8 @@
 '''
-this script is for badnet attack
+this script is for lf attack
+
+link : https://github.com/YiZeng623/frequency-backdoor
+The original license is placed at the end of this file.
 
 basic structure:
 1. config args, save_path, fix random seed
@@ -355,3 +358,28 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+'''
+MIT License
+
+Copyright (c) 2021 Yi Zeng
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
